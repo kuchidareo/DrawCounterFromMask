4324efc8ceb89915be874f1ec45f57dd0e6b6998
Reo Kuchida <kuchidareo@gmail.com>
2021-03-26 23:16:09 +0900

    膨張処理機能の追加

--- DrawCounterFromMask.py
+++ DrawCounterFromMask.py
@@ -48,6 +48,16 @@ def analyze():
                 ribeye_mask_img = cv2.imread(ribeye_mask_path,1)
                 ribeye_mask_img_gray = cv2.cvtColor(ribeye_mask_img, cv2.COLOR_BGR2GRAY)
 
+                image_height = ribeye_mask_img_gray.shape[0]
+                image_width = ribeye_mask_img_gray.shape[1]
+
+                for x in range(image_height):
+                    ribeye_mask_img_gray[x,0] = 0
+                    ribeye_mask_img_gray[x,image_width-1] = 0
+                for y in range(image_width):
+                    ribeye_mask_img_gray[0,y] = 0
+                    ribeye_mask_img_gray[image_height-1,y] = 0
+
                 #Sobelフィルタでx方向のエッジ検出
                 gray_sobelx = cv2.Sobel(ribeye_mask_img_gray,cv2.CV_32F,1,0)            
                 #Sobelフィルタでy方向のエッジ検出
@@ -57,13 +67,10 @@ def analyze():
                 gray_abs_sobely = cv2.convertScaleAbs(gray_sobely)
                 #重み付き和
                 ribeye_sobel_edge = cv2.addWeighted(gray_abs_sobelx,0.5,gray_abs_sobely,0.5,0)
+
                 ribeye_sobel_edge[ribeye_sobel_edge >= 100] = 255
                 ribeye_sobel_edge_green = np.zeros(cross_section_img.shape)
                 ribeye_sobel_edge_green[ribeye_sobel_edge >=254] = [0,255,0]
-
-                '''ribeye_rin= cv2.Canny(ribeye_mask_img_gray,100,200)
-                ribeye_rin_green = np.zeros(cross_section_img.shape)
-                ribeye_rin_green[:,:,1] = ribeye_rin'''
 
                 ribeye_mask_img = ribeye_mask_img / 255
                 cross_section_img_ribeye = cross_section_img * ribeye_mask_img
@@ -81,25 +88,16 @@ def analyze():
                 result_img = green_img.copy()
                 result_img[result_img < thresholding_value] = 0
                 result_img[result_img >= thresholding_value] = 255
+                kernel = np.ones((2,2),np.uint8)
+                result_img = cv2.dilate(result_img,kernel,iterations = 1)
+                result_img = (result_img / 255) * ribeye_mask_img * 255
+
                 result_img_rin = np.zeros(cross_section_img.shape)
+                result_img_rin = result_img + ribeye_sobel_edge_green
                 cross_section_img_rin = cross_section_img_255 + ribeye_sobel_edge_green
 
-
+                result_img_rin[ribeye_sobel_edge_green[:,:,1] >= 254] = [0,255,0]
                 cross_section_img_rin[ribeye_sobel_edge_green[:,:,1] >= 254] = [0,255,0]
-                ##cross_section_img_rin = cv2.merge([cross_section_img_rin_b, cross_section_img_rin_g, cross_section_img_rin_r])
-                ##cross_section_img_rin[:,:,2][cross_section_img_rin[:,:,1] >= 1] == 0
-
-                '''for x in range(ribeye_rin.shape[0]):
-                    for y in range(ribeye_rin.shape[1]):
-                        if ribeye_rin[x][y] == 0:
-                            continue
-                        else:
-                            cross_section_img_rin[x][y] = [0,255,0]'''
-
-                result_img_rin = result_img + ribeye_sobel_edge_green
-
-                
-
 
                 #path設定
                 original_path=cross_section_path.replace(os.sep,"/")
@@ -123,93 +121,6 @@ def analyze():
                 break
         continue
     finished()
-    '''ribeye_rin = np.zeros_like(ribeye_mask_img)
-
-    width = ribeye_mask_img.shape[0]
-    height = ribeye_mask_img.shape[1]
-
-    expand_rate = 200 / 100
-
-    ribeye_mask_img_expand = cv2.resize(ribeye_mask_img , (int(height*expand_rate), int(width*expand_rate)))
-    ribeye_mask_img_expand = ribeye_mask_img_expand[int(width*(expand_rate-1)/2) : int(width*(expand_rate+1)/2) , int(height*(expand_rate-1)/2) : int(height*(expand_rate+1)/2)]
-    expand_width = ribeye_mask_img_expand.shape[0]
-    expand_height = ribeye_mask_img_expand.shape[1]
-    ribeye_mask_img_expand = cv2.resize(ribeye_mask_img_expand , (height,width))
-
-    ribeye_rin = ribeye_mask_img_expand - ribeye_mask_img
-
-    cv2.imwrite("result.jpg",ribeye_rin)'''
-
-        
-        
-    '''for x in range(ribeye_mask_img.shape[0]-1):
-        for y in range(ribeye_mask_img.shape[1]-1):
-            if ribeye_mask_img_gray[x-1][y-1] == 0 and ribeye_mask_img_gray[x][y-1] == 0\
-            and ribeye_mask_img_gray[x][y+1] == 0 and ribeye_mask_img_gray[x+1][y] == 0\
-            and ribeye_mask_img_gray[x-1][y] == 0 and ribeye_mask_img_gray[x-1][y+1] == 0\
-            and ribeye_mask_img_gray[x+1][y-1] == 0 and ribeye_mask_img_gray[x+1][y+1] == 0 or \
-            ribeye_mask_img_gray[x-1,y-1] == 1 and ribeye_mask_img_gray[x,y-1] == 1\
-            and ribeye_mask_img_gray[x,y+1] == 1 and ribeye_mask_img_gray[x+1,y] == 1\
-            and ribeye_mask_img_gray[x-1,y] == 1 and ribeye_mask_img_gray[x-1,y+1] == 1\
-            and ribeye_mask_img_gray[x+1,y-1] == 1 and ribeye_mask_img_gray[x+1,y+1] == 1:
-                continue
-            else:
-                ribeye_rin[x][y] = 1
-        cv2.imwrite("result5.jpg",ribeye_rin)'''
-
-    
-
-
-    '''ribeye_mask_img_gray = cv2.cvtColor(ribeye_mask_img, cv2.COLOR_BGR2GRAY)
-    contours, _ = cv2.findContours(ribeye_mask_img_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    approx_contours = []
-    for i, cnt in enumerate(contours):
-        # 輪郭の周囲の長さを計算する。
-        arclen = cv2.arcLength(cnt, True)
-        # 輪郭を近似する。
-        approx_cnt = cv2.approxPolyDP(cnt, epsilon=0.00001 * arclen, closed=True)
-        approx_contours.append(approx_cnt)
-    ribeye_mask_contours_green = np.zeros_like(ribeye_mask_img)
-    cv2.drawContours(ribeye_mask_contours_green, approx_contours, -1, color=(0,255,0), thickness=1)
-    cv2.imwrite("test.jpg", ribeye_mask_contours_green)'''
-
-    '''for original_path in original_paths:
-        original_img= cv2.imread(original_path,1)
-        original_img = original_img / 255
-        imgBox = original_img[L[2]: L[3],L[0]: L[1]]
-        b = imgBox[:,:,0].flatten().mean()
-        g = imgBox[:,:,1].flatten().mean()
-        r = imgBox[:,:,2].flatten().mean()
-        print(b*255)
-        print(g*255)
-        print(r*255)
-        original_img[:, :, 0] = O[2] / 255 * (original_img[:, :, 0] / b)
-        original_img[:, :, 1] = O[1] / 255 * (original_img[:, :, 1] / g)
-        original_img[:, :, 2] = O[0] / 255 * (original_img[:, :, 2] / r)
-        original_img[original_img > 1] = 1
-        z = original_img / base_img
-        z[z>1] = 1
-        z = z * 255
-
-        #path設定
-        original_path=original_path.replace(os.sep,"/")
-        previous_path=original_path[:original_path.rfind("/")]
-        previous_path=previous_path[:previous_path.rfind("/")]
-        image_name=original_path[original_path.rfind("/")+1:]
-        image_name=image_name[:image_name.rfind(".")]
-        if not os.path.exists(previous_path+"/homography"):
-            os.mkdir(previous_path+"/homography")
-
-        
-        dst = []
-        dst = cv2.warpPerspective(z,M,(N[1],N[0]))
-        dst = dst.transpose(1,0,2)
-        dst = dst[::-1]
-        cv2.imwrite(previous_path+"/homography/"+image_name+"_homography.jpg", dst)
-        i+=1
-        q.set(str(i)+"/"+str(len(original_paths)))
-        del original_img,dst,z,imgBox
-    finished()'''
 
 def callback():
     button2.config(state="disable")
